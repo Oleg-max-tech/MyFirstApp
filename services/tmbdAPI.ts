8726350294596c80ab2e8138fee6b722bc81e60c
Oleg-max-tech <olehkostiv2003@gmail.com>
2024-11-28 17:01:38 +0200

    .

--- services/tmbdAPI.ts
+++ services/tmbdAPI.ts
@@ -2,36 +2,35 @@ export const API_KEY = "ea8cf142ec6b70fa2487438ddee56fca";
 export const BASE_URL = "https://api.themoviedb.org/3";
 
 export const tmbdApi = {
-  getTrendingMovies: async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
-    );
-    return response.json();
-  },
-  searchMovies: async (query: string) => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`
-    );
-    return response.json();
-  },
-  getMoviesByTypeAndGenre: async (type: string, genre: string | null) => {
-    const url = genre
-      ? `https://api.themoviedb.org/3/discover/${type}?with_genres=${genre}&api_key${API_KEY}`
-      : `https://api.themoviedb.org/3/trending/${type}/week?api_key=${API_KEY}`;
-    const response = await fetch(url);
-    return response.json();
-  },
-
-  getTrendingTVShows: async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}`
-    );
-    return response.json();
-  },
-  searchTVShows: async (query: string) => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/tv?query=${query}&api_key=${API_KEY}`
-    );
-    return response.json();
-  },
+  // getTrendingMovies: async () => {
+  //   const response = await fetch(
+  //     `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+  //   );
+  //   return response.json();
+  // },
+  // searchMovies: async (query: string) => {
+  //   const response = await fetch(
+  //     `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`
+  //   );
+  //   return response.json();
+  // },
+  // getMoviesByTypeAndGenre: async (type: string, genre: string | null) => {
+  //   const url = genre
+  //     ? `https://api.themoviedb.org/3/discover/${type}?with_genres=${genre}&api_key${API_KEY}`
+  //     : `https://api.themoviedb.org/3/trending/${type}/week?api_key=${API_KEY}`;
+  //   const response = await fetch(url);
+  //   return response.json();
+  // },
+  // getTrendingTVShows: async () => {
+  //   const response = await fetch(
+  //     `https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}`
+  //   );
+  //   return response.json();
+  // },
+  // searchTVShows: async (query: string) => {
+  //   const response = await fetch(
+  //     `https://api.themoviedb.org/3/search/tv?query=${query}&api_key=${API_KEY}`
+  //   );
+  //   return response.json();
+  // },
 };
